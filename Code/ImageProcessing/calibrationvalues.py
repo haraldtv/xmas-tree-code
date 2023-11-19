1dec59cc834d7a74aa596734a6539ad879a01291
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-11-19 15:27:43 +0100

    Added verison numbers to calibration values

--- Code/ImageProcessing/calibrationvalues.py
+++ Code/ImageProcessing/calibrationvalues.py
@@ -1,7 +1,9 @@
+# Kalibreringsverdier v0.8
 # def linearInterp():
 #     #                   x   -   y
 #     return [[141.632, 185.974], [29, 22]]
 
+# Kalibreringsverdier v0.9
 def linearInterp():
     #                   x   -   y
     return [[102.6, 186], [45, 25]]
